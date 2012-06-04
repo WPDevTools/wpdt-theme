b80ea42ab838c9c2deb41ac02f7ec673822db90f
Christopher Frazier <cfrazier@fraziermedia.com>
2012-06-04 14:21:14 -0700

    Initial code commit

---
+++ theme.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WPDT_Theme
+ *
+ * @version	0.1
+ * @package wpdt-theme
+ */
+
+if (!class_exists("WPDT_Theme")) :
+
+define('WPDT_THEME_VERSION', 0.1);
+
+/**
+ * WPDT_Theme
+ *
+ * Theme specific utility methods and structures
+ * 
+ * @author	Christopher Frazier (chris@wpdevtools.com), David Sutoyo (david@wpdevtools.com)
+ */
+class WPDT_Theme
+{
+
+} // WPDT_Theme
+
+
+endif; // Class check
